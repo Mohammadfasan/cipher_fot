6ee264a02118ef1bf5c37a448c83a8ae050a5f54
Raisary <raizisback@gmail.com>
2025-06-05 18:48:50 +0530

    decryption function added

--- main.c
+++ main.c
@@ -21,6 +21,29 @@ void encrypt(char temp[], char *encrypted, Queue *queue1) {
     encrypted[i] = '\0'; // null at end
 }
 
+//  decrypt function
+
+void decrypt(char temp[], char *decrypted, Queue *queue1) {
+    int i;
+    for (i = 0; temp[i] != '\0'; i++) {
+        if (temp[i] >= 'A' && temp[i] <= 'Z') {
+            char original = temp[i];
+            int index = getIndex(queue1, original);
+
+            if (index != -1) {
+                int newIndex = (index - 3 + MAXQUEUE) % MAXQUEUE;
+                decrypted[i] = getChar(queue1, newIndex); 
+            } else {
+                decrypted[i] = temp[i]; // Keep original if not found in queue
+            }
+        } else {
+            decrypted[i] = temp[i]; // Copy non-uppercase chars directly
+        }
+    }
+    decrypted[i] = '\0'; // at the end of srting
+}
+
+
 
 int main() {
     Queue alphabetQueue;
